523a90a9669bec43d96c64cbedb042d457cdfbec
kaden175ck <haoyang.shi@ucalgary.ca>
2024-03-11 00:45:49 -0600

    JS Basics01

---
+++ JS/script.js
@@ -0,0 +1,3 @@
+document.write("Hello World! You are in Script.js file");
+document.write("Never too late");
+alert("this file is working!");
---
+++ JS/script1.js
@@ -0,0 +1,23 @@
+document.write("<h2 style='color: blue;'>Content of JS!</h2>");
+document.write("<hr/>")
+
+//variables in JS
+var phrase = "To be or not to be, but never give up!";
+document.write(phrase);
+document.write(phrase);
+var phrase = 'Last Dance';
+
+
+// define few var
+var isMale = true;
+var age = 23;
+var age = 3.4;
+
+// Note: there is a difference between null and undefined
+// null means no value!
+// undefined means the var doesn't have a value YET!
+var flaws = null;
+var description = undefined
+
+
+document.write(phrase);
