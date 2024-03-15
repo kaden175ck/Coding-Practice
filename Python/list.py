108f29c126c87fa7e7b70d76aeeaaa32d49ae6a8
kaden175ck <haoyang.shi@ucalgary.ca>
2024-03-15 01:24:33 -0600

    list

---
+++ Python/list.py
@@ -0,0 +1,19 @@
+friends = ["Kv","Kr","Jm"]
+print(friends)
+
+print(friends[2])
+print(friends[0])
+
+print(friends[-1]) 
+print(friends[-2]) 
+# rememer the last position is always negative 1
+
+print(friends[1:])
+
+
+friends = ["Kv","Kr","Jm","Kdan","AB","BC"]
+print(friends[1:4])
+# not including 4, so it grab Kdan and end
+
+friends[1] = "Mike"
+print(friends[1])
