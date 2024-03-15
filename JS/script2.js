3022bf8653715d107bef081d0c401b808d9e80c8
kaden175ck <haoyang.shi@ucalgary.ca>
2024-03-15 00:57:42 -0600

    get element by id

--- JS/script2.js
+++ JS/script2.js
@@ -1,1 +1,14 @@
-document.getElementById("header");
+var header = document.getElementById("header");
+header.innerHTML = "The Java header got Overridden"
+header.style.color = "red";
+header.style.backgroundColor = "blue";
+
+var link = document.getElementById("link");
+link.href = "page.html"
+document.write("<br>");
+document.write("Above link got overridden to page, instead of index")
+// this above override the starting href, 
+// it was going to index
+// but now go to page
+document.write("<br>");
+link.style = "color: green;"
