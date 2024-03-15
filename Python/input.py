0727b687a051df9379b4c3ec9366ee972a033a60
kaden175ck <haoyang.shi@ucalgary.ca>
2024-03-14 22:06:46 -0600

    mad libs

---
+++ Python/input.py
@@ -0,0 +1,22 @@
+# name = input("Enter your name: ")
+# age = input("Enter your age: ")
+# print("Hello "+name+"!")
+
+# print("You are "+age+" years old!")
+
+num1 = input("Enter number 1: ")
+num2 = input("Enter number 2: ")
+result = float(num1) + float(num2)
+# user input is string!!!!
+# you are doing string concatenation
+# result = int(num1) + int(num2)
+print(result)
+
+
+color = input("enter a color : ")
+noun = input("enter a noun: ") 
+who = input("enter somebody: ")
+# mad libs game
+print("Roses are " + color)
+print(noun + " are blue")
+print("I love "+ who)
