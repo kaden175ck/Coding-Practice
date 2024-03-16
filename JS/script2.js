165082649aaa1be7a35f8b38167dc74c90340192
kaden175ck <haoyang.shi@ucalgary.ca>
2024-03-15 21:05:55 -0600

    event listener add

--- JS/script2.js
+++ JS/script2.js
@@ -1,5 +1,5 @@
 var header = document.getElementById("header");
-header.innerHTML = "The Java header got Overridden"
+header.innerHTML = "The Java header got Overridden by getelementId"
 header.style.color = "red";
 header.style.backgroundColor = "blue";
 
@@ -12,3 +12,26 @@ document.write("Above link got overridden to page, instead of index")
 // but now go to page
 document.write("<br>");
 link.style = "color: green;"
+
+
+function handleClick(element){
+    element.innerHTML = "Clicked"
+    element.style="background-color:blue;"
+}
+
+
+// add hover effect on image all in JS
+// function pass on par
+var image = document.getElementById("image");
+image.addEventListener("mouseover", function(){
+    this.style = "box-shadow: 10px 10px 10px yellow";
+    this.width = "400"
+
+});
+
+// so that it zoom in and zoom out when mouse hover
+image.addEventListener("mouseout", function(){
+    this.style = "";
+    this.width = "300"
+
+});
