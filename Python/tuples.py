bfb3b6d3a6fc983ff228f48850d135d1044fb074
kaden175ck <haoyang.shi@ucalgary.ca>
2024-03-15 18:08:53 -0600

    tuples

---
+++ Python/tuples.py
@@ -0,0 +1,14 @@
+coordinates = (12,24)
+# tupples cant be removed or modified , fixed
+
+print(coordinates[0])
+
+# coordinates[1]=10 will give errors
+
+# list use [], tuples use ()
+
+# you want to use tuples when you want to store data that never change
+# you can create a list of tuples
+
+coordinates_list = [(1,2),(3,4),(5,6)]
+# tuples still cant be changed
