df2233c53e14844462d92740a41e250d7e12401b
kaden175ck <haoyang.shi@ucalgary.ca>
2024-03-15 18:03:37 -0600

    list func

--- Python/list.py
+++ Python/list.py
@@ -17,3 +17,69 @@ print(friends[1:4])
 
 friends[1] = "Mike"
 print(friends[1])
+print("    /| ")
+print("   / | ")
+print("  /  | ")
+print(" /   | ")
+print("/____| ")
+
+
+lucky_numbers = [4, 8, 15, 16, 23, 42]
+friends = ["Kevin", "Karen", "Jim", "Oscar", "Toby"]
+
+
+friends.extend(lucky_numbers)
+print(friends)
+
+
+
+
+friends = ["Kevin", "Karen", "Jim", "Oscar", "Toby"]
+friends.append("Creed")
+print(friends)
+friends.insert(1,"Kelly")
+print(friends)
+
+
+print(friends)
+friends.remove("Jim")
+print(friends)
+friends.clear()
+print(friends)
+
+
+
+friends = ["Kevin", "Karen", "Jim", "Oscar", "Toby"]
+print(friends)
+friends.pop()#pop the last element in the list
+print(friends)
+
+
+print(friends.index("Kevin"))
+#print(friends.index("Toby")) just pop, will give error
+
+
+
+# count the number of appeareace
+print(friends.count("Kevin"))
+
+
+
+# sort this list
+friends.sort()
+print(friends)
+lucky_numbers.sort()
+print(lucky_numbers)
+lucky_numbers.reverse()
+print(lucky_numbers)
+
+print("    /| ")
+print("   / | ")
+print("  /  | ")
+print(" /   | ")
+print("/____| ")
+
+
+# copy list
+friends2 = friends.copy()
+print(friends2)
