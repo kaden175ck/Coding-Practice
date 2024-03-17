d10218025549bd2b6667950e8780d8e4d2a48aaf
kaden175ck <haoyang.shi@ucalgary.ca>
2024-03-17 17:37:05 -0600

    for and while do

---
+++ JS/loop.js
@@ -0,0 +1,35 @@
+var i = 1;
+
+while(i <= 10){
+    document.write(i + "<br>");
+    i++; //i=i+1
+}
+
+
+
+
+
+// do while loop will print 11 first before check condtion
+
+// var i = 11;
+// do{
+//     document.write(i + "<br>");
+//     i++; //i=i+1
+// }while(i <= 10)
+
+
+
+// For loop
+for(var j=0; j<20; j++){
+    document.write(j + "<br>");
+}
+
+//  ---
+document.write("<br>");
+document.write("<hr/>");
+var arrayFriends=["A","B","C","D"];
+//document.write(arrayFriends[1]+"<br>");
+
+for(var x=0; x < arrayFriends.length; x++){
+    document.write(arrayFriends[x]+"<br>")
+}
