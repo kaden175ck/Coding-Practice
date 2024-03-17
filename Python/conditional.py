a1936ddcf6c49529ae2093be751dbc48426a866c
kaden175ck <haoyang.shi@ucalgary.ca>
2024-03-16 20:57:35 -0600

    comparisons

--- Python/conditional.py
+++ Python/conditional.py
@@ -19,3 +19,26 @@ elif not(is_male) and is_tall:
 else:
     print("you are not male and you are not tall")
 
+
+
+
+
+# Comparisons
+    
+print("    /| ")
+print("   / | ")
+print("  /  | ")
+print(" /   | ")
+print("/____| ")
+
+# == , !=, all comparisons op
+def max_num(num1,num2,num3):
+    if num1 >= num2 and num1 >= num3:
+        return num1
+    elif num2 >= num1 and num2 >= num3:
+        return num2
+    else:
+        return num3
+
+print(max_num(3,4,5))
+             
