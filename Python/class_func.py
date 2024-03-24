69471ae944138c64f77186a23e7d5f9efe737555
kaden175ck <haoyang.shi@ucalgary.ca>
2024-03-23 21:23:42 -0600

    object functions

---
+++ Python/class_func.py
@@ -0,0 +1,8 @@
+from student_class import Student
+
+student1 = Student("A", "CSA", 3.1)
+student2 = Student("B", "CSB", 3.9)
+
+
+print(student1.on_honor_roll())
+print(student2.on_honor_roll())
--- Python/student_class.py
+++ Python/student_class.py
@@ -3,3 +3,10 @@ class Student:
         self.name = name
         self.major = major
         self.gpa = gpa
+
+# class function can be used as obejct as the class
+    def on_honor_roll(self):
+        if self.gpa >= 3.5:
+            return True
+        else:
+            return False
