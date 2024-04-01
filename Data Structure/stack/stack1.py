97399d298cbdeda64a943d8dff7ec288e99ac9b3
kaden175ck <haoyang.shi@ucalgary.ca>
2024-03-31 20:37:01 -0600

    update datastructure

---
+++ Data Structure/stack/stack1.py
@@ -0,0 +1,33 @@
+# Problem Description:
+
+# You're given a string that might contain these parentheses: '(', ')', '{', '}', '[', ']'. Your task is to figure out if the order of these parentheses is correct.
+
+# Example:
+
+# Input: "()[]{}"
+# Output: True (The parentheses match correctly)
+# Input: "(]"
+# Output: False (Mismatched parentheses)
+
+def is_valid(st):
+    stack = []
+    mapping = { 
+        ")" : "(",
+        "]" : "[",
+        "}" : "{"       
+    }
+
+    # check each char in the string
+    for char in st:
+
+    # not stack means to check if the stack is empty
+    # also check if mapping char equlas to the first thing on the stack
+    
+    # we only run when char in the mapping, if not in mapping, then just return not stack
+        if char in mapping:
+            if not stack or mapping[char] != stack.pop():
+                return False
+            else:
+                stack.append(char)
+            
+    return not stack
---
+++ Data Structure/stack/stack2.py
@@ -0,0 +1,12 @@
+# Problem Description:
+
+# Reverse Polish Notation (RPN) is a different way to write math expressions. For example:
+
+# Standard: 10 + 2
+# RPN: 10 2 +
+# Your job is to get an input in RPN format and calculate the result.
+
+# Example
+
+# Input: ["2", "1", "+", "3", "*"]
+# Output: 9 (Explanation: ((2 + 1) * 3) = 9)
