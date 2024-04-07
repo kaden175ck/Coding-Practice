77f139b722bf911233dfd9c81a132c3d305dae95
kaden175ck <haoyang.shi@ucalgary.ca>
2024-04-07 00:28:49 -0600

    stack done

--- Data Structure/stack/stack3.py
+++ Data Structure/stack/stack3.py
@@ -1,6 +1,11 @@
 # **Problem Description**
 
-# You're given an array `temperatures` where `temperatures[i]` is the daily temperature on day `i`. Your task is to return an array where each element represents how many days you have to wait until a warmer temperature occurs. If there's no future day with a warmer temperature, put a 0 in that position.
+# You're given an array `temperatures` 
+# where `temperatures[i]` is the daily temperature on day `i`. 
+# Your task is to return an array 
+# where each element represents how many days you have to wait 
+# until a warmer temperature occurs. 
+# If there's no future day with a warmer temperature, put a 0 in that position.
 
 # **Example:**
 
@@ -8,3 +13,25 @@
 # * **Output:** [1, 1, 4, 2, 1, 1, 0, 0]
 
 # **Stack-based Solution**
+
+def daily_temperatures(temperatures):
+    result = [0] * len(temperatures)
+    stack = []  # Stack of (temperature, index) pairs
+
+    for i, temp in enumerate(temperatures): # index, then temperature, (index, temperature)
+        while stack and temp > stack[-1][0]:  
+            # -1 get the last element, this case is the top element in the stack
+            # because we are storing tuples in stack
+            # 0 would give the temperature, 1 would give the indexs
+            # so you are comparing the temp
+
+
+            _, prev_index = stack.pop()
+            # choosing to ignore the first element in the tuple, which is temp,
+            # pop the index, the index part is assigned to prev_index
+
+            result[prev_index] = i - prev_index
+        stack.append((temp, i))
+        # this tuples are stored in the stack
+
+    return result
---
+++ Data Structure/stack/stack_teach.py
@@ -0,0 +1,55 @@
+# stack = []  # Create an empty stack
+
+# stack.append(10)  
+# stack.append(20)  
+# stack.append(30)  
+
+# print(stack.pop())  # Output: 30
+# print(stack[-1])    # Output: 20 (peek)
+# print(len(stack))   # Output: 2
+
+
+
+
+class Stack:
+    def __init__(self):
+        self.items = []
+
+    def push(self, item):
+        self.items.append(item)
+
+    def pop(self):
+        if not self.items:  # Check if empty
+            raise Exception("Stack is empty")
+        return self.items.pop()
+
+    def peek(self):
+        if not self.items:
+            raise Exception("Stack is empty")
+        return self.items[-1]
+
+    def isEmpty(self):
+        return self.items == []
+
+    def size(self):
+        return len(self.items)
+
+
+# Create a stack and add elements
+my_stack = Stack() 
+my_stack.push(5)
+my_stack.push(10)
+my_stack.push("hello") 
+
+# Remove and retrieve the top item
+top_item = my_stack.pop()
+print("Popped item:", top_item)  # Output: Popped item: hello
+
+# Check the current top item 
+current_top = my_stack.peek()
+print("Current top item:", current_top) # Output: Current top item: 10
+
+# Get the stack size
+stack_size = my_stack.size()
+print("Stack size:", stack_size)  # Output: Stack size: 2
+
