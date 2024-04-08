717de3a88590a2a5ec11707941f056b969f65b1c
kaden175ck <haoyang.shi@ucalgary.ca>
2024-04-08 17:15:14 -0600

    queue

--- Data Structure/queue/queue1.py
+++ Data Structure/queue/queue1.py
@@ -3,13 +3,14 @@
 
 
 
-# This article dives into how to create a stack using just queues. 
+# how to create a stack using just queues. 
 # A stack, you might recall, follows the LIFO (Last In, First Out) principle, 
 # similar to a stack of plates. The one you add last is the first one you can remove.
 
 
 
-# * Design a data structure that simulates the functionality of a stack using only queue operations (enqueue, dequeue, etc.).
+# * Design a data structure that simulates the functionality of a stack 
+# using only queue operations (enqueue, dequeue, etc.).
 
 # **What is a Stack?**
 
@@ -22,26 +23,24 @@
 
 # **The Challenge**
 
-# * **Mismatch:** Queues follow FIFO (First-In, First-Out). The challenge is to reverse this behavior using only queue operations to achieve LIFO. 
-
-# **What the Code Needs to Do**
+# * **Mismatch:** Queues follow FIFO (First-In, First-Out). 
+# The challenge is to reverse this behavior using only queue operations to achieve LIFO. 
 
 # Your code must provide the following class, implementing the functionalities as described:
 
-# ```python
-# class MyStack:
-#     def __init__(self):
-#         # Initialize your data structure here.
 
-#     def push(self, x: int) -> None:
-#         # Push element x onto stack.
+class MyStack:
+    def __init__(self):
+        # Initialize your data structure here.
 
-#     def pop(self) -> int:
-#         # Removes the element on top of the stack and returns that element.
+    def push(self, x: int) -> None:
+        # Push element x onto stack.
 
-#     def top(self) -> int:
-#         # Get the top element.
+    def pop(self) -> int:
+        # Removes the element on top of the stack and returns that element.
 
-#     def empty(self) -> bool:
-#         # Returns whether the stack is empty.
-# ```
+    def top(self) -> int:
+        # Get the top element.
+
+    def empty(self) -> bool:
+        # Returns whether the stack is empty.
