321045036b420405340571286064c2305980d40d
kaden175ck <haoyang.shi@ucalgary.ca>
2024-04-11 22:19:58 -0600

    queue2

--- Data Structure/queue/queue2.py
+++ Data Structure/queue/queue2.py
@@ -1,1 +1,4 @@
-# * **Implement Queue using Stacks ([https://leetcode.com/problems/implement-queue-using-stacks/](https://leetcode.com/problems/implement-queue-using-stacks/))**   The counterpart to the previous problem, this reinforces how you can manipulate data structures to achieve different functionalities.
+# * **Implement Queue using Stacks ([https://leetcode.com/problems/implement-queue-using-stacks/]
+#(https://leetcode.com/problems/implement-queue-using-stacks/))**   
+#The counterpart to the previous problem, 
+#this reinforces how you can manipulate data structures to achieve different functionalities.
