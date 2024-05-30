3089bd0fbf583a4fe6e0335524783a04d75903ca
kaden175ck <haoyang.shi@ucalgary.ca>
2024-05-30 15:20:27 +0800

    set up questions

---
+++ Data Structure/arrays/array1.py
@@ -0,0 +1,4 @@
+# 1. **Find Numbers with Even Number of Digits (Easy)**
+#    - **Problem**: Given an array `nums` of integers, return how many of them contain an even number of digits.
+#    - **Example**: If the input is `nums = [12, 345, 2, 6, 7896]`, the output should be `2` (because 12 and 7896 have an even number of digits).
+
---
+++ Data Structure/arrays/arrays5.py
@@ -0,0 +1,5 @@
+# 5. **Check If N and Its Double Exist (Easy)**
+#    - **Problem**: Given an array `arr` of integers, check if there exists two indices i and j such that:
+#      - `i != j`
+#      - `arr[i] == 2 * arr[j]`
+#    - **Example**: Input: `arr = [10,2,5,3]`, Output: `true` (Because 10 is twice as 5, which is an element at a different index).
