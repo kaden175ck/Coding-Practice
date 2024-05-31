a3ad9afb9943708913d33fcbe93f918acf581bfa
kaden175ck <haoyang.shi@ucalgary.ca>
2024-05-31 11:07:06 +0800

    queue update

--- Data Structure/queue/queue4.py
+++ Data Structure/queue/queue4.py
@@ -1,3 +1,4 @@
-# * **Open the Lock  ([https://leetcode.com/problems/open-the-lock/]
+# Open the Lock  ([https://leetcode.com/problems/open-the-lock/]
 # Another excellent BFS problem using a queue to explore possible combinations 
-# and find the shortest path.
+# and find the shortest path.你有一个转盘锁，每个转盘有 0 到 9 十个数字。给定一个死锁数字的列表，
+# 你需要找到从 "0000" 到目标密码的最少转动次数，但不能转到死锁数字。这个问题可以用队列实现广度优先搜索。
--- Data Structure/queue/queue5.py
+++ Data Structure/queue/queue5.py
@@ -1,9 +1,9 @@
-# **2. Number of Islands (Medium)**
-
-#    * **Problem:** [https://leetcode.com/problems/number-of-islands/]
-# (https://leetcode.com/problems/number-of-islands/)
-#    * **Approach:**  Employ a queue for Breadth-First Search (BFS).  
+# Number of Islands (Medium)
+# [https://leetcode.com/problems/number-of-islands/]
+# Employ a queue for Breadth-First Search (BFS).  
 # Start from a land cell ('1'), enqueue it, and within a loop, 
 # dequeue and explore neighboring land cells while marking them as visited.  
 # Repeat for every unvisited land cell to find all distinct islands.
+# 这个问题要求计算一个由 1（陆地）和 0（水）组成的二维网格中岛屿的数量。
+# 可以通过使用队列实现广度优先搜索来遍历每个岛屿。
 
