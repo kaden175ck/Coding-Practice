de04213e1f1ba03262d64cab09dff53f330d32e2
kaden175ck <haoyang.shi@ucalgary.ca>
2024-05-31 10:10:13 +0800

    questions

---
+++ Data Structure/arrays/arrays7.py
@@ -0,0 +1,3 @@
+# 2. **Maximum Subarray (Easy)**:
+#    - **Problem**: Find the contiguous subarray (containing at least one number) which has the largest sum and return its sum.
+#    - **Example**: If the input array is `[-2,1,-3,4,-1,2,1,-5,4]`, the contiguous subarray `[4,-1,2,1]` has the largest sum = 6.
---
+++ Data Structure/arrays/arrays9.py
@@ -0,0 +1,3 @@
+# 4. **Product of Array Except Self (Medium)**:
+#    - **Problem**: Given an array `nums` of n integers where n > 1, return an array output such that output[i] is equal to the product of all the elements of `nums` except `nums[i]`.
+#    - **Constraint**: Solve it without using division and in O(n).
